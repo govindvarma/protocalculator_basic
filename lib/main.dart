4f9543b56121c573735ae4f23d7618823f8f0eb9
Govind <gov.varma@gmail.com>
2022-01-19 12:42:10 -0500

    implementation with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,256 +1,178 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CalcModel(),
+      child: MyApp(),
+    )
+  );
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Calculator(),
+      title: 'Calculator Demo',
+      home: Scaffold(body: MyCalc())
     );
   }
 }
 
-class Calculator extends StatefulWidget {
+// keyboard of the calculator
+final _keyboardList = ["7","8","9","/","4","5","6","x","1","2","3","-","AC","0","=","+"];
+
+class MyCalc extends StatefulWidget {
+  const MyCalc({ Key? key }) : super(key: key);
+
   @override
-  _CalculatorState createState() => _CalculatorState();
+  _MyCalcState createState() => _MyCalcState();
 }
 
-class _CalculatorState extends State<Calculator> {
-  dynamic displaytxt = 20;
-  //Button Widget
-  Widget calcbutton(String btntxt, Color btncolor, Color txtcolor) {
-    return Container(
-      child: FlatButton(
-        onPressed: () {
-          calculation(btntxt);
-        },
-        child: Text(
-          '$btntxt',
-          style: TextStyle(
-            fontSize: 35,
-            color: txtcolor,
-          ),
-        ),
-        shape: CircleBorder(),
-        color: btncolor,
-        padding: EdgeInsets.all(20),
-      ),
-    );
-  }
+class _MyCalcState extends State<MyCalc> {
 
   @override
   Widget build(BuildContext context) {
-    //Calculator
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 5),
+    final _calculator = Provider.of<CalcModel>(context);
+
+    return Container(
+      child: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            // Calculator display
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      '$text',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 100,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('AC', Colors.white, Colors.black),
-                calcbutton('+/-', Colors.white, Colors.black),
-                calcbutton('%', Colors.white, Colors.black),
-                calcbutton('/', Colors.white, Colors.black),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('7', Colors.white, Colors.black),
-                calcbutton('8', Colors.white, Colors.black),
-                calcbutton('9', Colors.white, Colors.black),
-                calcbutton('x', Colors.white, Colors.black),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('4', Colors.white, Colors.black),
-                calcbutton('5', Colors.white, Colors.black),
-                calcbutton('6', Colors.white, Colors.black),
-                calcbutton('-', Colors.white, Colors.black),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('1', Colors.white, Colors.black),
-                calcbutton('2', Colors.white, Colors.black),
-                calcbutton('3', Colors.white, Colors.black),
-                calcbutton('+', Colors.white, Colors.black),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                //this is button Zero
-                FlatButton(
-                  padding: EdgeInsets.fromLTRB(34, 20, 128, 20),
-                  onPressed: () {
-                    calculation('0');
-                  },
-                  shape: StadiumBorder(),
-                  child: Text(
-                    '0',
-                    style: TextStyle(fontSize: 35, color: Colors.black),
-                  ),
-                  color: Colors.white,
+            Container(
+              color: Colors.grey.shade200,
+              alignment: Alignment.bottomRight,
+              padding: const EdgeInsets.only( right: 30.0, bottom: 10),
+              height: 200,
+              child: Text(
+                '${_calculator.value}',
+                maxLines: 1,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 50,
                 ),
-                calcbutton('.', Colors.white, Colors.black),
-                calcbutton('=', Colors.white, Colors.black),
-              ],
+              )
             ),
-            SizedBox(
-              height: 10,
-            ),
-          ],
-        ),
-      ),
+            Flexible(
+              child: GridView.count(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                crossAxisCount: 4,
+                childAspectRatio: 0.7,
+                children: List.generate(_keyboardList.length, (index) {
+                  return InkWell(
+                    onTap: (){
+                      _calculator._onKeyTapped('${_keyboardList[index]}');
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '${_keyboardList[index]}',
+                        style: TextStyle(
+                          fontSize: 25,
+                        )
+                      )
+                    )
+                  );
+                })
+              )
+            )
+          ]),
+      )
     );
   }
+}
 
-  //Calculator logic
-  dynamic text = '0';
-  double numOne = 0;
-  double numTwo = 0;
+class CalcModel extends ChangeNotifier {
+    String _current = "0";
+    String _previous = "";
+    String _operation = "";
+    bool _isResult = false;
+    String get value => _current;
 
-  dynamic result = '';
-  dynamic finalResult = '';
-  dynamic opr = '';
-  dynamic preOpr = '';
-  void calculation(btnText) {
-    if (btnText == 'AC') {
-      text = '0';
-      numOne = 0;
-      numTwo = 0;
-      result = '';
-      finalResult = '0';
-      opr = '';
-      preOpr = '';
-    } else if (opr == '=' && btnText == '=') {
-      if (preOpr == '+') {
-        finalResult = add();
-      } else if (preOpr == '-') {
-        finalResult = sub();
-      } else if (preOpr == 'x') {
-        finalResult = mul();
-      } else if (preOpr == '/') {
-        finalResult = div();
-      }
-    } else if (btnText == '+' ||
-        btnText == '-' ||
-        btnText == 'x' ||
-        btnText == '/' ||
-        btnText == '=') {
-      if (numOne == 0) {
-        numOne = double.parse(result);
-      } else {
-        numTwo = double.parse(result);
-      }
+  // Arithmetic
+  _onKeyTapped(String text){
+    switch (text){
+      case "+":
+      case "-":
+      case "x":
+      case "/":
+        _isResult = false;
+        _operation = text;
+        break;
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        if (_isResult) {
+          _current = text;
+          _previous = "";
+          _operation = "";
+          notifyListeners();
+          break;
+        }
+        if (_operation.isNotEmpty && _previous.isEmpty) {
+          _previous = _current;
+          _current = "";
+        }
+        _current += text;
+        if (_current.startsWith('0') && _current != '0'){
+          _current = _current.substring(1);
+        }
+        notifyListeners();
+        break;
+      case "=":
+        double val1 = _toDouble(_previous);
+        double val2 = _toDouble(_current);
+        switch (_operation) {
+          case "+":
+            _current = "${val1 + val2}";
+            break;
+          case "-":
+            _current = "${val1 - val2}";
+            break;
+          case "x":
+            _current = "${val1 * val2}";
+            break;
+          case "/":
+            _current = "${val1 / val2}";
+            break;   
+        }
+        if (_isInteger(_toDouble(_current))){
+          _current = ("${_toDouble(_current).toInt()}");
+        }
+        _previous = "";
+        _isResult = true;
+        _operation = "";
+        notifyListeners();
+        break;
+      case "AC":
+        _current = "0";
+        _previous = "";
+        _isResult = false;
+        _operation = "";
+        notifyListeners();
+        break;
+    }
+  }
 
-      if (opr == '+') {
-        finalResult = add();
-      } else if (opr == '-') {
-        finalResult = sub();
-      } else if (opr == 'x') {
-        finalResult = mul();
-      } else if (opr == '/') {
-        finalResult = div();
-      }
-      preOpr = opr;
-      opr = btnText;
-      result = '';
-    } else if (btnText == '%') {
-      result = numOne / 100;
-      finalResult = doesContainDecimal(result);
-    } else if (btnText == '.') {
-      if (!result.toString().contains('.')) {
-        result = result.toString() + '.';
-      }
-      finalResult = result;
-    } else if (btnText == '+/-') {
-      result.toString().startsWith('-')
-          ? result = result.toString().substring(1)
-          : result = '-' + result.toString();
-      finalResult = result;
+  double _toDouble(String val){
+    if (val.startsWith('-')) {
+      String s = val.substring(1);
+      return double.parse(s) * -1;
     } else {
-      result = result + btnText;
-      finalResult = result;
+      return double.parse(val);
     }
-
-    setState(() {
-      text = finalResult;
-    });
   }
 
-  String add() {
-    result = (numOne + numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
+  bool _isInteger(num val) => (val % 1) == 0;
 
-  String sub() {
-    result = (numOne - numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String mul() {
-    result = (numOne * numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String div() {
-    result = (numOne / numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String doesContainDecimal(dynamic result) {
-    if (result.toString().contains('.')) {
-      List<String> splitDecimal = result.toString().split('.');
-      if (!(int.parse(splitDecimal[1]) > 0))
-        return result = splitDecimal[0].toString();
-    }
-    return result;
-  }
 }
